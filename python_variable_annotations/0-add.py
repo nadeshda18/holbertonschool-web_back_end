e29cf516bc683034bca9b812aca43e0ac32f6d75
nadeshda18 <nadeshda02@hotmail.com>
2024-01-01 16:44:10 -0400

    check 3 fail

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""function add to return their sums as a float"""
+"""function add to return their sums
+as a float"""
 
 
 def add(a, b):
-    """function add to return their sums as a float"""
+    """function add to return their sums
+    as a float"""
     return float(a + b)
