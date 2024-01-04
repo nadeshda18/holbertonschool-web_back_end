724f915bfc1edc1eb4519fc6f2865da7d7a151e3
nadeshda18 <nadeshda02@hotmail.com>
2024-01-04 16:21:33 -0400

    check fail 6

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,8 +11,9 @@ module = import_module(module_name)
 wait_random = module.wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """execute multiple coroutines at the same
     time with async"""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    tasks = (wait_random(max_delay) for _ in range(n))
+    lst = await asyncio.gather(*tasks)
+    return sorted(lst)
