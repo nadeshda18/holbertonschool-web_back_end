59e609658d149423c809d56f353fb28b990140cd
nadeshda18 <nadeshda02@hotmail.com>
2024-01-04 16:24:36 -0400

    added

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""coroutine that takes no arguments"""
+"""coroutine async_generator that takes no arguments"""
 import asyncio
 import random
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """coroutine that takes no arguments"""
-    for _ in range(10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
