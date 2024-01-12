77e6f9b6347a3fec62838badaae3ae07ddd1ba59
nadeshda18 <nadeshda02@hotmail.com>
2024-01-12 08:23:47 -0400

    no idea

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,6 +6,9 @@ index and an end index"""
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """return tuple"""
-    return ((page - 1) * page_size, page * page_size)
+def index_range(page, page_size):
+    """ return a tuple """
+    if page and page_size:
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
