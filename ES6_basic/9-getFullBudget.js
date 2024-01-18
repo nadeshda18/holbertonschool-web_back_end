0f80f4da240e4c267bb115f0d7c029c0c68316ee
nadeshda18 <nadeshda02@hotmail.com>
2024-01-18 03:25:04 -0400

    lint

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-/* eslint import/extensions: ['error', 'ignorePackages', { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' }] */ // eslint-disable-line
+/* eslint-disable */
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
