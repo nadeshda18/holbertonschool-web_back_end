87a7e878daaf2844238e5a0e85112338cae4bc49
nadeshda18 <nadeshda02@hotmail.com>
2024-01-18 15:36:18 -0400

    airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+// 7-airport.js
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
