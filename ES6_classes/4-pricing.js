144caa9eb487b835c76623dae4ec20fd2613e11e
nadeshda18 <nadeshda02@hotmail.com>
2024-01-18 03:48:54 -0400

    ok

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+// 4-pricing.js
+// eslint-disable-next-line import/extensions
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
