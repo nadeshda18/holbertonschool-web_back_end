7fded896c1b0b18a475453aab31beea5ed3e0fde
nadeshda18 <nadeshda02@hotmail.com>
2024-01-18 14:59:39 -0400

    lint error

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,9 +1,9 @@
 // 2-hbtn_course.js
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, 'Name');
-    this._length = this.validateNumber(length, 'Length');
-    this._students = this.validateArray(students, 'Students');
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
@@ -11,7 +11,8 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    this._name = this.validateString(newName, 'Name');
+    this.validateString(newName);
+    this._name = newName;
   }
 
   get length() {
@@ -19,7 +20,8 @@ export default class HolbertonCourse {
   }
 
   set length(newLength) {
-    this._length = this.validateNumber(newLength, 'Length');
+    this.validateNumber(newLength);
+    this._length = newLength;
   }
 
   get students() {
@@ -27,26 +29,27 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    this._students = this.validateArray(newStudents, 'Students');
+    this.validateArray(newStudents);
+    this._students = newStudents;
   }
 
-  validateString(value, attributeName) {
+  validateString(value) {
     if (typeof value !== 'string') {
-      throw new TypeError(`${attributeName} must be a string`);
+      throw new TypeError('Name must be a string');
     }
     return value;
   }
 
-  validateNumber(value, attributeName) {
+  validateNumber(value) {
     if (typeof value !== 'number' || Number.isNaN(value)) {
-      throw new TypeError(`${attributeName} must be a number`);
+      throw new TypeError('Length must be a number');
     }
     return value;
   }
 
-  validateArray(value, attributeName) {
-    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) {
-      throw new TypeError(`${attributeName} must be an array of strings`);
+  validateArray(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
     }
     return value;
   }
