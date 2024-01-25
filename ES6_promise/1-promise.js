98a217d8a46e2ba30ed31faca9b90b3e5bcb7c1b
nadeshda18 <nadeshda02@hotmail.com>
2024-01-25 19:39:35 -0400

    another promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject('The fake API is not working correctly');
+    }
+  });
+}
+export default getFullResponseFromAPI;
