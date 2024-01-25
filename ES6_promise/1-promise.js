9fafbe99c1eae2754aedfc782f6a55f93c875ef7
nadeshda18 <nadeshda02@hotmail.com>
2024-01-25 19:47:59 -0400

    little changes

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      reject('Error: The fake API is not working correctly');
+      reject(new Error('The fake API is not working correctly'));
     }
   });
 }
-export default getFullResponseFromAPI;
