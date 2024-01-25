48611b8180cbe1916183594e53c3a7475a4e8088
nadeshda18 <nadeshda02@hotmail.com>
2024-01-25 19:58:06 -0400

    eslint correction

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
+    // eslint-disable-next-line
     .then((result) => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
+    // eslint-disable-next-line
     .catch((error) => {
       console.error('An error occured');
       return new Error();
