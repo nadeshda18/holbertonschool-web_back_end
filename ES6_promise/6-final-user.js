8c5c2a0bb90beb1acb96176993ee359adcd1f400
nadeshda18 <nadeshda02@hotmail.com>
2024-01-25 20:15:53 -0400

    handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstname, lastname, filename) {
+  return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
+    .then(
+      (value) => ({ status: 'success', value }),
+      (error) => ({ status: 'error', error }),
+    );
+}
