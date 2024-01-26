0007ce5202345a529e42a33efa2e1f5b53dddf36
nadeshda18 <nadeshda02@hotmail.com>
2024-01-26 11:32:41 -0400

    maybe like this

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
-    .then((value) => ({ status: 'success', value }), (error) => ({ status: 'error', error }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+    })));
 }
