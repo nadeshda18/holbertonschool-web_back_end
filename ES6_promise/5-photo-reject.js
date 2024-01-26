7f3d0d0e222b5e4b7569d413bc4fedd23df5ac11
nadeshda18 <nadeshda02@hotmail.com>
2024-01-26 08:04:45 -0400

    like this

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
-export default function uploadPhoto(fileName) {
-  return Promise.reject(Error(`${fileName} cannot be processed`));
+export default function uploadPhoto(filename) {
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  });
 }
--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export function handleProfileSignup(firstname, lastname, filename) {
+export default function handleProfileSignup(firstname, lastname, filename) {
   return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
-  .then(value => ({status: 'success', value: value}), error => ({status: 'error', error: error}))
+    .then((value) => ({ status: 'success', value }), (error) => ({ status: 'error', error }));
 }
